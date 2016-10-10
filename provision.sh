3c3c3c68f9e0ffcbc2a9d891014864dbfc4db49d
Rob Phoenix <robertphoenix@gmail.com>
2016-10-10 13:44:07 +0100

    improve handling off playbooks repo

--- provision.sh
+++ provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# User Variables
 LOCALE=en_GB.UTF-8
 TIMEZONE=Europe/London
 GITHUB_USER=bordeltabernacle
 PLAYBOOKS_DIR=ansible_network_automation_poc
+VAGRANT_HOME=/home/vagrant
+
 
 echo "+-------------------------------------------------+"
 echo "| Provisioning Ansible Networking Vagrant Machine |"
@@ -10,12 +13,11 @@ echo "+-------------------------------------------------+"
 
 echo "Setting locale & timezone..."
 sudo locale-gen $LOCALE > /dev/null 2>&1
-echo export LC_ALL=$LOCALE >> /home/vagrant/.profile
-echo export TZ=$TIMEZONE >> /home/vagrant/.profile
+echo export LC_ALL=$LOCALE >> $VAGRANT_HOME/.profile
+echo export TZ=$TIMEZONE >> $VAGRANT_HOME/.profile
 
 echo "Creating library & tmp Directories..."
-mkdir /home/vagrant/library > /dev/null 2>&1
-mkdir /home/vagrant/tmp > /dev/null 2>&1
+mkdir $VAGRANT_HOME/tmp > /dev/null 2>&1
 
 echo "Adding Ansible Repo..."
 sudo apt-add-repository ppa:ansible/ansible > /dev/null 2>&1
@@ -32,21 +34,23 @@ sudo apt-get -y install git > /dev/null 2>&1
 echo "Installing Ansible..."
 sudo apt-get -y install ansible > /dev/null 2>&1
 
-echo "Installing Pip & Requirements"
+echo "Installing Pip"
 sudo apt-get -y install python-pip > /dev/null 2>&1
-sudo pip install -r /home/vagrant/requirements.txt > /dev/null 2>&1
 
 echo "Installing NTC Ansible Module..."
-git clone https://github.com/networktocode/ntc-ansible --recursive > /dev/null 2>&1
-sudo mv /home/vagrant/ntc-ansible/library/* /home/vagrant/library/ > /dev/null 2>&1
+sudo rm -r $VAGRANT_HOME/shared/ntc-ansible > /dev/null 2>&1
+git clone https://github.com/networktocode/ntc-ansible --recursive $VAGRANT_HOME/shared/ntc-ansible > /dev/null 2>&1
 
 echo "Installing Napalm Ansible Module..."
-git clone https://github.com/napalm-automation/napalm-ansible > /dev/null 2>&1
-sudo mv /home/vagrant/napalm-ansible/library/* /home/vagrant/library/ > /dev/null 2>&1
+sudo rm -r $VAGRANT_HOME/shared/napalm-ansible > /dev/null 2>&1
+git clone https://github.com/napalm-automation/napalm-ansible $VAGRANT_HOME/shared/napalm-ansible > /dev/null 2>&1
 
-echo "Cloning & Symlinking $GITHUB_USER/$PLAYBOOKS_DIR..."
-git clone https://github.com/$GITHUB_USER/$PLAYBOOKS_DIR.git /home/vagrant/shared/src > /dev/null 2>&1
-ln -snf /home/vagrant/library /home/vagrant/shared/src/library > /dev/null 2>&1
+echo "Cloning $GITHUB_USER/$PLAYBOOKS_DIR..."
+sudo rm -r $VAGRANT_HOME/shared/src > /dev/null 2>&1
+git clone https://github.com/$GITHUB_USER/$PLAYBOOKS_DIR.git $VAGRANT_HOME/shared/src > /dev/null 2>&1
+
+echo "Installing $GITHUB_USER/$PLAYBOOKS_DIR requirements..."
+sudo -H pip install -r $VAGRANT_HOME/shared/src/requirements.txt > /dev/null 2>&1
 
 echo "+------------------------------------------------+"
 echo "| Ansible Networking Vagrant Machine Provisioned |"

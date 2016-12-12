5269ac2fadc8aab5fbb9eadbd97232d495e165a3
Rob Phoenix <robphoenix@protonmail.com>
2016-12-12 14:09:33 +0000

    don't install ansible via apt

--- provision.sh
+++ provision.sh
@@ -12,37 +12,32 @@ echo "| Provisioning Ansible Networking Vagrant Machine |"
 echo "+-------------------------------------------------+"
 
 echo "Setting locale & timezone..."
-sudo locale-gen $LOCALE > /dev/null 2>&1
+sudo locale-gen $LOCALE
 echo export LC_ALL=$LOCALE >> $VAGRANT_HOME/.profile
 echo export TZ=$TIMEZONE >> $VAGRANT_HOME/.profile
 
-echo "Creating library & tmp Directories..."
-mkdir $VAGRANT_HOME/tmp > /dev/null 2>&1
+echo "Creating tmp Directory..."
+mkdir $VAGRANT_HOME/tmp
 
-echo "Adding Ansible Repo..."
-sudo apt-add-repository ppa:ansible/ansible > /dev/null 2>&1
+echo "Updating apt..."
+sudo apt -y update
 
-echo "Updating apt-get..."
-sudo apt-get -y update > /dev/null 2>&1
+# echo "Installing Dependencies..."
+# sudo apt-get -y install software-properties-common zlib1g-dev libxml2-dev libxslt-dev build-essential libssl-dev libffi-dev python-dev
 
-echo "Installing Dependencies..."
-sudo apt-get -y install software-properties-common zlib1g-dev libxml2-dev libxslt-dev build-essential libssl-dev libffi-dev python-dev > /dev/null 2>&1
-
-echo "Installing Git..."
-sudo apt-get -y install git > /dev/null 2>&1
-
-echo "Installing Ansible..."
-sudo apt-get -y install ansible > /dev/null 2>&1
+# echo "Installing Git..."
+# sudo apt-get -y install git
 
 echo "Installing Pip"
-sudo apt-get -y install python-pip > /dev/null 2>&1
+sudo apt-get -y install python-pip
+sudo -H pip install -U pip
 
 echo "Cloning $GITHUB_USER/$PLAYBOOKS_DIR..."
-sudo rm -r $VAGRANT_HOME/shared/src > /dev/null 2>&1
-git clone https://github.com/$GITHUB_USER/$PLAYBOOKS_DIR.git $VAGRANT_HOME/shared/src > /dev/null 2>&1
+sudo rm -r $VAGRANT_HOME/shared/src
+git clone https://github.com/$GITHUB_USER/$PLAYBOOKS_DIR.git $VAGRANT_HOME/shared/src
 
 echo "Installing $GITHUB_USER/$PLAYBOOKS_DIR requirements..."
-sudo -H pip install -r $VAGRANT_HOME/shared/src/requirements.txt > /dev/null 2>&1
+sudo -H pip install -r $VAGRANT_HOME/shared/src/requirements.txt
 
 echo "+------------------------------------------------+"
 echo "| Ansible Networking Vagrant Machine Provisioned |"
